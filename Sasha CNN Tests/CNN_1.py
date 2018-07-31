4de25f18fae03a7f80a8f9a4cc740e183df478af
sashabii <Sashabii@hotmail.com>
2018-07-31 11:55:54 +0200

    Updated files from my side

---
+++ Sasha CNN Tests/CNN_1.py
@@ -0,0 +1,111 @@
+''' Adapted from tutorial found at:
+https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+'''
+
+# Perform random transformations on data to prevent overfitting
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+# datagen = ImageDataGenerator(
+#         rotation_range=40,
+#         width_shift_range=0.2,
+#         height_shift_range=0.2,
+#         rescale=1./255,
+#         shear_range=0.2,
+#         zoom_range=0.2,
+#         horizontal_flip=True,
+#         fill_mode='nearest')
+
+# img = load_img('data\\train\\angry\\ang-1-0.png')  # this is a PIL image
+# x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+# x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+
+# # the .flow() command below generates batches of randomly transformed images
+# # and saves the results to the `preview/` directory
+# i = 0
+# for batch in datagen.flow(x, batch_size=1,
+#                           save_to_dir='preview', save_prefix='angry', save_format='png'):
+#     i += 1
+#     if i > 20:
+#         break  # otherwise the generator would loop indefinitely
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+# dimensions of our images.
+img_width, img_height = 1092, 315
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 400
+nb_validation_samples = 160
+epochs = 50
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(1092, 315,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering="th"))
+
+model.add(Conv2D(32, (3, 3), dim_ordering="th"))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering="th"))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering="th"))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+batch_size = 16
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is a generator that will read pictures found in
+# subfolers of 'data/train', and indefinitely generate
+# batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        'data/train',  # this is the target directory
+        target_size=(1092, 315),  # all images will be resized to 150x150
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        'data/validation',
+        target_size=(1092, 315),
+        batch_size=batch_size,
+        class_mode='binary')
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size)
+model.save_weights('first_try.h5')  # always save your weights after training or during training
